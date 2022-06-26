932deecc2408aa5b2fd1f6f37fb531c706abe09a
MelvinLopez00089 <98369987+MelvinLopez00089@users.noreply.github.com>
2022-06-26 17:43:24 -0600

    proyecto 1
     Menu de juegos

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+#include "startShip.h"
+
+#include "snake.h"
+
+
+
+using namespace std;
+
+int main (int argc, char const *argv[])
+{
+
+	int opcion = 0;
+
+	
+	
+
+	while(true)
+
+	{
+
+		system("cls");
+
+		cout << "MELVIN YOVANI LOPEZ" << endl;
+		
+
+		cout << "**************" << endl;
+
+		cout << "MENU DE JUEGOS" << endl;
+
+		cout << "**************" << endl;
+
+		cout << endl;
+
+		
+
+		cout << "Selecione un juego" << endl;
+
+		cout << "1 - startShip" << endl;
+
+		cout << "2 - snake" << endl;
+
+		cout << endl;
+
+		
+
+		cout << "Ingrese un numero del menu para seleccionar un juego: ";
+
+		cin >> opcion;
+
+		
+
+		switch(opcion)
+
+		{
+
+			case 1:
+
+				system ("cls");
+
+				starShip();
+
+				break;
+
+			case 2:
+
+				system ("cls");
+
+				snake();
+				
+			default:
+				break;
+		}
+
+		
+		
+			}
+			return 0;
+}
